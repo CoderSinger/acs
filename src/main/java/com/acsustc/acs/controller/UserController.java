a7b10ba63b5d9be62a2a66c670c80f136a8047cb
CoderSinger <chen_zhenxiu@126.com>
2018-11-24 02:41:27 +0800

    修改了cdr_table_a的主键uuid的生成策略

--- src/main/java/com/acsustc/acs/controller/UserController.java
+++ src/main/java/com/acsustc/acs/controller/UserController.java
@@ -1,21 +1,17 @@
 package com.acsustc.acs.controller;
 
 import com.acsustc.acs.entity.User;
-import com.acsustc.acs.repository.UserRepository;
 import com.acsustc.acs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserRepository repository;
 
     @Autowired
     private UserService userService;
