b112e34d6c699bc3e4eb014d94e94361db23dee3
CoderSinger <chen_zhenxiu@126.com>
2018-11-24 02:19:50 +0800

    完成了登录注册功能 完成了对cdr_table_a和cdr_table_b的数据库增删查改的开发

--- src/main/java/com/acsustc/acs/controller/UserController.java
+++ src/main/java/com/acsustc/acs/controller/UserController.java
@@ -4,17 +4,15 @@ import com.acsustc.acs.entity.User;
 import com.acsustc.acs.repository.UserRepository;
 import com.acsustc.acs.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServlet;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@RestController
-@RequestMapping("/user/*")
+@Controller
 public class UserController {
     @Autowired
     private UserRepository repository;
@@ -24,76 +22,89 @@ public class UserController {
 
     private User user = new User();
 
-    @RequestMapping("/say")
-    public String say()
-    {
-        return "hello";
+    /**
+     * 域名访问重定向
+     * 作用：输入域名后重定向到index（首页）
+     * */
+    @RequestMapping("")
+    public String index(HttpServletResponse response) {
+        //重定向到 /index
+        return response.encodeRedirectURL("/index");
     }
 
-
+    /**
+     * 首页API
+     * 作用：显示首页
+     * */
     @RequestMapping("/index")
-    public String index(HttpSession httpSession) {
-        httpSession.getAttribute("userLogin");
+    public String home(Model model) {
+        //对应到templates文件夹下面的index
         return "index";
     }
 
-
-    @RequestMapping("/register")
-    public String register() {
-        return "register";
+    //解决“Request method 'GET' not supported”问题
+    @RequestMapping(value = "/register" , method = RequestMethod.GET)
+    public String toRegister(){
+        return  "register";
+    }
+    //解决“Request method 'GET' not supported”问题
+    @RequestMapping(value = "/login" , method = RequestMethod.GET)
+    public String toLogin(){
+        return  "login";
+    }
+    /**
+     * 注册API
+     * @method：post
+     * @param user（从View层传回来的user对象）
+     * @return 重定向
+     * */
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerPost(Model model,
+                               //这里和模板中的th:object="${user}"对应起来
+                               @ModelAttribute(value = "user") User user,
+                               HttpServletResponse response) {
+        //我们可以用Sout这种最原始打印方式来检查数据的传输
+        System.out.println("Controller信息:"+user.getUserName());
+        System.out.println("Controller密码:"+user.getPassword());
+        //使用userService处理业务
+        String result = userService.register(user);
+        //将结果放入model中，在模板中可以取到model中的值
+        //这里就是交互的一个重要地方，我们可以在模板中通过这些属性值访问到数据
+        model.addAttribute("result", result);
+        //开始重定向，携带数据过去了。
+        return response.encodeRedirectURL("/index");
     }
 
-    @RequestMapping("/login")
-    public String login() {
-        String str = "";
-        String username = user.getUserName();
-        if(username !=  null) {
-            str = "index";
-        }else {
-            str = "login";
+    /**
+     * 登录API
+     * @method：post
+     * @param user（从View层传回来的user对象）
+     * @return 重定向
+     * */
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPost(Model model,
+                            @ModelAttribute(value = "user1") User user,
+                            HttpServletResponse response,
+                            HttpSession session) {
+        System.out.println(user.getUserName() + "!!!!");
+        String result = userService.login(user);
+        if (result.equals("登录成功")) {
+            //session是作为用户登录信息保存的存在
+            session.setAttribute("user",user);
         }
-        return str;
+        model.addAttribute("result", result);
+        return response.encodeRedirectURL("/index");
     }
-
-
-    @RequestMapping("/ulogin")
-    public String login(HttpServletRequest request, HttpSession session) {
-        String username = request.getParameter("userName");
-        String password = request.getParameter("password");
-        user = userService.login(username, password);
-
-        String str = "";
-        if(user != null) {
-            session.setAttribute("userLogin", user);
-            str = "index";
-        }else {
-            str = "login";
-        }
-        return str;
+    /**
+     * 注销API
+     * @method：get
+     * @return 首页
+     * */
+    @RequestMapping(value = "/loginOut", method = RequestMethod.GET)
+    public String loginOut(HttpSession session) {
+        //从session中删除user属性，用户退出登录
+        session.removeAttribute("user");
+        return "index";
     }
-
-
-    @RequestMapping("/uregister")
-    public String register(HttpServletRequest request) {
-        String userName = request.getParameter("userName");
-        String password = request.getParameter("password");
-        String password2 = request.getParameter("password2");
-        String phoneNumber = request.getParameter("phoneNumber");
-        String str = "";
-        if(password.equals(password2)) {
-            user = userService.select(userName);
-            if(user  ==  null) {
-                User user = new User(userName, password, phoneNumber);
-                userService.insert(user);
-                str = "login";
-            }else {
-                str = "register";
-            }
-        }else {
-            str = "register";
-        }
-        return str;
-    }
-
-
 }
+
